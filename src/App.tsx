f89142faeb9971f53f3cbe172ba6b89adf6bff86
khennyboy <abdullateefkehinde848@gmail.com>
2026-03-04 17:25:58 +0100

    add changes to the chat iput file

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 import HomePage from "./pages/homepage";
 import MenuProvider from "./context/menu-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,9 +28,11 @@ function App() {
   return (
     <>
       <MenuProvider>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
+        {/* <ChakraProvider> */}
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+          </QueryClientProvider>
+        {/* </ChakraProvider> */}
       </MenuProvider>
     </>
   );
