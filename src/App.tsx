fb85602e416da2eebcef449c02cf240b3c2df8cd
khennyboy <abdullateefkehinde848@gmail.com>
2026-03-11 08:57:37 +0100

    made significant progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,43 @@
-  import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-    RouterProvider,
-  } from "react-router-dom";
-  import HomePage from "./pages/homepage";
-  import MenuProvider from "./context/menu-provider";
-  import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-  import { Provider } from "./components/ui/provider";
-  import Conversation from "./ui/conversation";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/homepage";
+import MenuProvider from "./context/menu-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "./components/ui/provider";
+import Conversation from "./ui/conversation";
 
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 0,
-      },
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
     },
-  });
+  },
+});
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<HomePage />}>
-          <Route path=":conversationId" element={<Conversation />} />
-        </Route>
-      </>,
-    ),
-  );
-  function App() {
-    return (
-      <>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<HomePage />}>
+      <Route index element={null} />
+      <Route path=":conversationId" element={<Conversation />} />
+    </Route>,
+  ),
+);
+function App() {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
         <MenuProvider>
           <Provider>
-            <QueryClientProvider client={queryClient}>
-              <RouterProvider router={router} />
-            </QueryClientProvider>
+            <RouterProvider router={router} />
           </Provider>
         </MenuProvider>
-      </>
-    );
-  }
+      </QueryClientProvider>
+    </>
+  );
+}
 
-  export default App;
+export default App;
